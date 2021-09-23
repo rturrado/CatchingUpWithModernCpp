7e867620e4f2ee86e71d4180d179d7ab3cf8b429
rturrado <rturrado@gmail.com>
2021-09-23 18:32:06 +0200

    - StackOverflowQuestions:   * Added PopenTest module.

--- StackOverflowQuestions/src/Main.cpp
+++ StackOverflowQuestions/src/Main.cpp
@@ -7,6 +7,8 @@
 #include "PlayingWithTimeZonesAndDst.h"
 #include "VectorOfIntsWithTrianglePattern.h"
 
+import PopenTest;
+
 int main()
 {
 /*
@@ -18,9 +20,11 @@ int main()
     { std::cout << "[PARSE INI FILE WITH BOOST]\n"; parse_ini_file_with_boost_main(); std::cout << "\n"; }
     // Vector of ints with triangle pattern
     { std::cout << "[VECTOR OF INTS WITH TRIANGLE PATTERN]\n"; vector_of_ints_with_triangle_pattern_main(); std::cout << "\n"; }
-*/
     // Parse lines of dates and times
     { std::cout << "[PARSE LINES OF DATES AND TIMES]\n"; parse_lines_of_dates_and_times_main(); std::cout << "\n"; }
     // Playing with time zones and DST
     { std::cout << "[PLAYING WITH TIME ZONES AND DST]\n"; playing_with_time_zones_and_dst_main(); std::cout << "\n"; }
+*/
+    // Popen test
+    { std::cout << "[POPEN TEST]\n"; popen_test_main(); std::cout << "\n"; }
 }
