bcf455a172c97e8dc6d83ebeec0f772312fb3e0a
rturrado <rturrado@gmail.com>
2021-11-10 00:04:16 +0100

    - TheModernC++Challenge:   * Changed problem 61 to test the parallel transform function with different thread pool sizes and block sizes.   * Changed the implementation to use a pool of task slots.

--- TheModernC++Challenge/src/Chapter7_Concurrency/Problem61.cpp
+++ TheModernC++Challenge/src/Chapter7_Concurrency/Problem61.cpp
@@ -8,34 +8,57 @@
 #include <future>  // async
 #include <iostream>  // cout
 #include <numeric>  // iota
+#include <semaphore>  // counting_semaphore
+#include <thread>  // hardware_concurrency
 #include <vector>
 
+static const size_t THREAD_POOL_SIZE_DEFAULT{ std::thread::hardware_concurrency() };
+static const size_t THREAD_POOL_SIZE_MAX{ std::thread::hardware_concurrency() * 2 };
+static const size_t BLOCK_SIZE_DEFAULT{ 10'000 };
+static const size_t BLOCK_SIZE_MIN{ 100 };
+
 template <typename InputIt, typename OutputIt, typename UnaryOperation>
-void parallel_transform(InputIt first, InputIt last, OutputIt d_first, UnaryOperation&& f)
+void parallel_transform(InputIt first, InputIt last, OutputIt d_first, UnaryOperation&& f,
+    size_t thread_pool_size = THREAD_POOL_SIZE_DEFAULT, size_t block_size = BLOCK_SIZE_DEFAULT)
 {
     size_t size{ static_cast<size_t>(std::distance(first, last)) };
     
-    const size_t THREAD_POOL_SIZE_MAX{ 4 };
-    const size_t BLOCK_SIZE_MIN{ 100 };
+    thread_pool_size = std::min(thread_pool_size, THREAD_POOL_SIZE_MAX);
+    block_size = std::max(block_size, BLOCK_SIZE_MIN);
 
-    size_t thread_pool_size{ std::max(std::min(size / BLOCK_SIZE_MIN, static_cast<size_t>(1)), static_cast<size_t>(4)) };
-    size_t block_size{ size / thread_pool_size };
+    std::counting_semaphore task_slots(thread_pool_size);
 
-    auto futures{ std::vector<std::future<void>>(thread_pool_size) };
-
-    for (size_t i{ 0 }; i < thread_pool_size; ++i)
+    size_t num_blocks{ (size / block_size) + (size % block_size) };
+    auto futures{ std::vector<std::future<void>>(num_blocks) };
+    
+    // Launch one task per block
+    // We can run thread_pool_size tasks in parallel
+    // If all task slots are busy, we have to wait for a task to finish
+    for (size_t i{ 0 }; i < num_blocks; ++i)
     {
+        auto begin_it{ first + i * block_size };
+        auto end_it{ first + std::min(size, (i + 1) * block_size) };
+        auto d_begin_it{ d_first + i * block_size };
+
+        // Wait for a task slot to be free
+        task_slots.acquire();
+
         futures[i] = std::async(
             std::launch::async,
-            [first, d_first, i, block_size, size, &f]() {
+            [begin_it, end_it, d_begin_it, &f, &task_slots]() {
+                // Execute task
                 std::transform(
-                    first + i * block_size,
-                    first + std::min(size, (i + 1) * block_size),
-                    d_first + i * block_size,
+                    begin_it,
+                    end_it,
+                    d_begin_it,
                     std::forward<UnaryOperation>(f));
+                // Release a task slot
+                task_slots.release();
             }
         );
     }
+    
+    // Wait for all tasks to finish
     for (auto& ft: futures) { ft.get(); }
 }
 
@@ -50,28 +73,43 @@ void problem_61_main()
     
     std::vector<int> v(10'000'000);
     std::iota(std::begin(v), std::end(v), 0);
+
+    std::cout << "Times:";
+    // std::transform
     auto w{ v };
-    auto x{ v };
-
-    auto t1{ function_timer<>::duration(
-        [&v, &square]() mutable {
-            parallel_transform(std::cbegin(v), std::cend(v), std::begin(v), square);
+    {
+        auto t{ function_timer<>::duration(
+            [&w, &square]() mutable {
+                std::transform(std::cbegin(w), std::cend(w), std::begin(w), square);
+            }
+        ) };
+        std::cout << std::format("\n\tstd::transform: {}", t);
+    }
+    // std::transform(std::execution::par)
+    {
+        auto w{ v };
+        auto t{ function_timer<>::duration(
+            [&w, &square]() mutable {
+                std::transform(std::execution::par, std::cbegin(w), std::cend(w), std::begin(w), square);
+            }
+        ) };
+        std::cout << std::format("\n\tstd::transform(ex::par): {}", t);
+    }
+    // Parallel transform with different thread pool and block sizes
+    for (size_t thread_pool_size : { 4, 8, 16 })
+    {
+        for (size_t block_size : { 1'000, 10'000, 100'000 })
+        {
+            auto x{ v };
+            auto t{ function_timer<>::duration(
+                [thread_pool_size, block_size, &x, &square]() mutable {
+                    parallel_transform(std::cbegin(x), std::cend(x), std::begin(x), square, thread_pool_size, block_size);
+                }
+            ) };
+            assert("Error: vectors are different" and w == x);
+            std::cout << std::format("\n\tparallel_transform(thread_pool_size={}, block_size={}): {}", thread_pool_size, block_size, t);
         }
-    ) };
-    auto t2{ function_timer<>::duration(
-        [&w, &square]() mutable {
-            std::transform(std::cbegin(w), std::cend(w), std::begin(w), square);
-        }
-    ) };
-    auto t3{ function_timer<>::duration(
-        [&x, &square]() mutable {
-            std::transform(std::execution::par, std::cbegin(x), std::cend(x), std::begin(x), square);
-        }
-    ) };
+    }
 
-    assert("Error: vectors are different" and v == w);
-
-    std::cout << std::format("Times:\n\tparallel_transform: {}\n\tstd::transform: {}\n\tstd::transform(ex::par): {}\n", t1, t2, t3);
-
-    std::cout << "\n";
+    std::cout << "\n\n";
 }
