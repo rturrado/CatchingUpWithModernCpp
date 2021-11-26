929dc73811d9c262bf6a1eef154d05aa34644eca
rturrado <rturrado@gmail.com>
2021-11-26 01:15:31 +0100

    - TheModernC++Challenge:   * Implemented a second solution for the problem 67.

--- TheModernC++Challenge/src/Chapter8_DesignPatterns/Problem67.cpp
+++ TheModernC++Challenge/src/Chapter8_DesignPatterns/Problem67.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>  // all_of, any_of
 #include <cctype>  // isalpha, isdigit, islower, isupper
 #include <iostream>  // cout
+#include <memory>  // make_unique, unique_ptr
 #include <optional>
 #include <string>
 #include <string_view>
@@ -16,58 +17,131 @@ using validate_return_type = std::optional<validate_error_message>;
 //
 // The PasswordStrengthValidator base class:
 //   - specifies a 'validate' interface,
-//   - holds a pointer to the next validator in the chain, offering a setter for it, and
-//   - lets validators to continue the validation process through a 'validate_with_next' method
+//   - holds a raw pointer to the next validator in the chain, offering a setter for it, and
+//   - lets validators continue the validation process through a 'validate_with_next' method
 //
 // The different concrete classes:
 //   - implement the validate interface, and
 //   - whether they return an error or they return control to the base class to continue with the validation
 //
-class PasswordStrengthValidator
+// The client:
+//   - has to create the validators separately, then
+//   - chain them using the 'set_next' base class method (passing the address of each validator as argument), and, finally,
+//   - call 'validate' on the first of them
+namespace password_strength_validator_v1
 {
-public:
-    virtual validate_return_type validate(std::string_view pw) const noexcept = 0;
+    class PasswordStrengthValidator
+    {
+    public:
+        virtual validate_return_type validate(std::string_view pw) const noexcept = 0;
 
-    void set_next(PasswordStrengthValidator* next) noexcept { next_ = next; }
-protected:
-    PasswordStrengthValidator* next_{ nullptr };
+        void set_next(PasswordStrengthValidator* next) noexcept { next_ = next; }
+    protected:
+        PasswordStrengthValidator* next_{ nullptr };
 
-    [[nodiscard]] validate_return_type validate_with_next(std::string_view pw) const noexcept {
-        return (next_ ? next_->validate(pw) : validate_return_type{});
-    }
-};
+        [[nodiscard]] validate_return_type validate_with_next(std::string_view pw) const noexcept {
+            return (next_ ? next_->validate(pw) : validate_return_type{});
+        }
+    };
 
 
-class MinimumLengthValidator : public PasswordStrengthValidator
+    class MinimumLengthValidator : public PasswordStrengthValidator
+    {
+    public:
+        MinimumLengthValidator() = default;
+        explicit MinimumLengthValidator(size_t length) : length_{ length } {}
+
+        [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
+            if (pw.size() < length_) { return std::string{ "password length has to be at least " } + std::to_string(length_); }
+            else { return validate_with_next(pw); };
+        }
+    private:
+        size_t length_{ 8 };
+    };
+
+
+    template <typename Predicate>
+    class ContainsValidator : public PasswordStrengthValidator
+    {
+    public:
+        ContainsValidator() = delete;
+        explicit ContainsValidator(Predicate pred, std::string_view error_message) : pred_{ pred }, error_message_{ error_message } {}
+
+        [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
+            if (not pred_(pw)) { return error_message_; }
+            else { return validate_with_next(pw); };
+        }
+    private:
+        Predicate pred_{};
+        std::string error_message_{};
+    };
+}  // namespace password_strength_validator_v1
+
+
+// This solution is a variant on the previous one, and a bit more similar to the book's solution
+//
+// The PasswordStrengthValidator base class:
+//   - specifies a 'validate' interface,
+//   - holds a unique pointer to the next validator in the chain, which receives as a constructor argument, and
+//   - lets validators continue the validation process through a 'validate_with_next' method
+//
+// The different concrete classes:
+//   - implement the validate interface, and
+//   - whether they return an error or they return control to the base class to continue with the validation
+//
+// The client:
+//   - creates all the validators on the go, chaining them via their constructors, and
+//   - calls 'validate' on the outermost one
+namespace password_strength_validator_v2
 {
-public:
-    MinimumLengthValidator() = default;
-    explicit MinimumLengthValidator(size_t length) : length_{ length } {}
+    class PasswordStrengthValidator
+    {
+    public:
+        virtual validate_return_type validate(std::string_view pw) const noexcept = 0;
 
-    [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
-        if (pw.size() < length_) { return std::string{"password length has to be at least "} + std::to_string(length_); }
-        else { return validate_with_next(pw); };
-    }
-private:
-    size_t length_{8};
-};
+    protected:
+        std::unique_ptr<PasswordStrengthValidator> next_{ nullptr };
+
+        explicit PasswordStrengthValidator(std::unique_ptr<PasswordStrengthValidator> next) : next_{ std::move(next) } {}
+
+        [[nodiscard]] validate_return_type validate_with_next(std::string_view pw) const noexcept {
+            return (next_ ? next_->validate(pw) : validate_return_type{});
+        }
+    };
 
 
-template <typename Predicate>
-class ContainsValidator : public PasswordStrengthValidator
-{
-public:
-    ContainsValidator() = delete;
-    explicit ContainsValidator(Predicate pred, std::string_view error_message) : pred_{ pred }, error_message_{ error_message } {}
+    class MinimumLengthValidator : public PasswordStrengthValidator
+    {
+    public:
+        MinimumLengthValidator() = default;
+        MinimumLengthValidator(size_t length, std::unique_ptr<PasswordStrengthValidator> next)
+            : length_{ length }, PasswordStrengthValidator{ std::move(next) } {}
 
-    [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
-        if (not pred_(pw)) { return error_message_; }
-        else { return validate_with_next(pw); };
-    }
-private:
-    Predicate pred_{};
-    std::string error_message_{};
-};
+        [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
+            if (pw.size() < length_) { return std::string{ "password length has to be at least " } + std::to_string(length_); }
+            else { return validate_with_next(pw); };
+        }
+    private:
+        size_t length_{ 8 };
+    };
+
+
+    template <typename ContainsOrErrorF>
+    class ContainsValidator : public PasswordStrengthValidator
+    {
+    public:
+        ContainsValidator() = delete;
+        ContainsValidator(ContainsOrErrorF f, std::unique_ptr<PasswordStrengthValidator> next)
+            : contains_or_error_f_{ f }, PasswordStrengthValidator{ std::move(next) } {}
+
+        [[nodiscard]] validate_return_type validate(std::string_view pw) const noexcept override {
+            if (auto error{ contains_or_error_f_(pw) }) { return error.value(); }
+            else { return validate_with_next(pw); };
+        }
+    private:
+        ContainsOrErrorF contains_or_error_f_{};
+    };
+}  // namespace password_strength_validator_v2
 
 
 // Validating passwords
@@ -100,23 +174,65 @@ void problem_67_main()
         );
     };
 
-    for (auto&& pw : { "hola"sv, "holaquetal"sv, "holaqueta!"sv, "h0laqueta!"sv, "H0LAQUETA!"sv, "h0laQueTa!"sv })
+    // v1
     {
-        MinimumLengthValidator validator{ 8 };
-        ContainsValidator contains_symbol_validator{ contains_symbol, "password has to contain at least one 'symbol'"sv };
-        ContainsValidator contains_digit_validator{ contains_digit, "password has to contain at least one 'digit'"sv };
-        ContainsValidator contains_lowercase_validator{ contains_lowercase, "password has to contain at least one 'lowercase' letter"sv };
-        ContainsValidator contains_uppercase_validator{ contains_uppercase, "password has to contain at least one 'uppercase' letter"sv };
+        using namespace password_strength_validator_v1;
 
-        validator.set_next(&contains_symbol_validator);
-        contains_symbol_validator.set_next(&contains_digit_validator);
-        contains_digit_validator.set_next(&contains_lowercase_validator);
-        contains_lowercase_validator.set_next(&contains_uppercase_validator);
+        std::cout << "v1:\n";
+        for (auto&& pw : { "hola"sv, "holaquetal"sv, "holaqueta!"sv, "h0laqueta!"sv, "H0LAQUETA!"sv, "h0laQueTa!"sv })
+        {
+            MinimumLengthValidator validator{ 8 };
+            ContainsValidator contains_symbol_validator{ contains_symbol, "password has to contain at least one 'symbol'"sv };
+            ContainsValidator contains_digit_validator{ contains_digit, "password has to contain at least one 'digit'"sv };
+            ContainsValidator contains_lowercase_validator{ contains_lowercase, "password has to contain at least one 'lowercase' letter"sv };
+            ContainsValidator contains_uppercase_validator{ contains_uppercase, "password has to contain at least one 'uppercase' letter"sv };
 
-        auto validator_result{ validator.validate(pw) };
-        if (not validator_result) { std::cout << "'" << pw << "' is a valid password.\n"; }
-        else { std::cout << "'" << pw << "' is not valid: " << validator_result.value() << ".\n"; }
+            validator.set_next(&contains_symbol_validator);
+            contains_symbol_validator.set_next(&contains_digit_validator);
+            contains_digit_validator.set_next(&contains_lowercase_validator);
+            contains_lowercase_validator.set_next(&contains_uppercase_validator);
+
+            if (auto error{ validator.validate(pw) }) { std::cout << "\t'" << pw << "' is not valid: " << error.value() << ".\n"; }
+            else { std::cout << "\t'" << pw << "' is a valid password.\n"; }
+        }
+        std::cout << "\n";
     }
 
-    std::cout << "\n";
+    // v2
+    {
+        using namespace password_strength_validator_v2;
+
+        auto contains_symbol_or_error = [&contains_symbol](std::string_view sv) {
+            return (contains_symbol(sv) ? validate_return_type{} : validate_error_message{ "password has to contain at least one 'symbol'" });
+        };
+        auto contains_digit_or_error = [&contains_digit](std::string_view sv) {
+            return (contains_digit(sv) ? validate_return_type{} : validate_error_message{ "password has to contain at least one 'digit'" });
+        };
+        auto contains_lowercase_or_error = [&contains_lowercase](std::string_view sv) {
+            return (contains_lowercase(sv) ? validate_return_type{} : validate_error_message{ "password has to contain at least one 'lowercase'" });
+        };
+        auto contains_uppercase_or_error = [&contains_uppercase](std::string_view sv) {
+            return (contains_uppercase(sv) ? validate_return_type{} : validate_error_message{ "password has to contain at least one 'uppercase'" });
+        };
+
+        std::cout << "v2:\n";
+        for (auto&& pw : { "hola"sv, "holaquetal"sv, "holaqueta!"sv, "h0laqueta!"sv, "H0LAQUETA!"sv, "h0laQueTa!"sv })
+        {
+            auto validator(
+                std::make_unique<MinimumLengthValidator>(8,
+                    std::make_unique<ContainsValidator<decltype(contains_symbol_or_error)>>(contains_symbol_or_error,
+                        std::make_unique<ContainsValidator<decltype(contains_digit_or_error)>>(contains_digit_or_error,
+                            std::make_unique<ContainsValidator<decltype(contains_lowercase_or_error)>>(contains_lowercase_or_error,
+                                std::make_unique<ContainsValidator<decltype(contains_uppercase_or_error)>>(contains_uppercase_or_error, nullptr)
+                            )
+                        )
+                    )
+                )
+            );
+
+            if (auto error{ validator->validate(pw) }) { std::cout << "\t'" << pw << "' is not valid: " << error.value() << ".\n"; }
+            else { std::cout << "\t'" << pw << "' is a valid password.\n"; }
+        }
+        std::cout << "\n";
+    }
 }
