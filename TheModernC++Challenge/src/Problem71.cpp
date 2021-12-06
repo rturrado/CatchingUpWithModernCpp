f33d6ed570ffd92ce0f116ab65034b93162cb0b5
rturrado <rturrado@gmail.com>
2021-12-06 16:01:37 +0100

    - TheModernC++Challenge:   * Continued working on problem 71.

--- TheModernC++Challenge/src/Problem71.cpp
+++ TheModernC++Challenge/src/Problem71.cpp
@@ -42,11 +42,11 @@ public:
     [[nodiscard]] NotificationType get_type() const noexcept { return type_; }
     [[nodiscard]] size_t get_index_of_changed_element() const noexcept { return index_of_changed_element_; }
 private:
+    static inline size_t id{};
+
     size_t id_{};
     NotificationType type_{};
     size_t index_of_changed_element_{};
-
-    static inline size_t id{ 0 };
 };
 
 std::ostream& operator<<(std::ostream& os, const Notification& n)
@@ -59,13 +59,13 @@ std::ostream& operator<<(std::ostream& os, const Notification& n)
 }
 
 
-class IdGenerator
+class GroupIdGenerator
 {
 public:
-    static IdGenerator& get_instance() { static IdGenerator instance; return instance; }
+    static GroupIdGenerator& get_instance() { static GroupIdGenerator instance; return instance; }
     size_t operator()(const std::string& group) { return cache_[group]++; }
 private:
-    IdGenerator() = default;
+    GroupIdGenerator() = default;
 
     using group_t = std::string;
     using id_t = size_t;
@@ -89,7 +89,8 @@ protected:
     explicit Observer(std::shared_ptr<ObservableVector<T>> sp_ov) noexcept
         : sp_ov_{ sp_ov }, id_{ id_generator_("Observer") } {}
 private:
-    static inline IdGenerator& id_generator_{ IdGenerator::get_instance() };
+    static inline GroupIdGenerator& id_generator_{ GroupIdGenerator::get_instance() };
+
     size_t id_{};
 };
 
@@ -99,14 +100,14 @@ template <typename T>
 class ConcreteObserver1 : public Observer<T>
 {
 public:
-    explicit ConcreteObserver1(std::shared_ptr<ObservableVector<T>> sp_ov) noexcept : Observer<T>::Observer(sp_ov) {}
+    explicit ConcreteObserver1(std::shared_ptr<ObservableVector<T>> sp_ov) noexcept : Observer<T>::Observer{ sp_ov } {}
 protected:
     virtual void update(const Notification& n) const noexcept override
     {
         auto id{ Observer<T>::get_id() };
         auto& ov{ Observer<T>::get_observable_vector() };
-        std::cout << "[Observer " << id  << "] received notification: " << n << "\n";
-        std::cout << "[Observer " << id << "] observable vector: " << ov << "\n";
+        std::cout << "\t[Observer " << id << "] received notification: " << n << "\n";
+        std::cout << "\t[Observer " << id << "] observable vector " << ov.get_id() << ": " << ov << "\n";
     }
 };
 
@@ -115,14 +116,14 @@ template <typename T>
 class ConcreteObserver2 : public Observer<T>
 {
 public:
-    explicit ConcreteObserver2(std::shared_ptr<ObservableVector<T>> sp_ov) noexcept : Observer<T>::Observer(sp_ov) {}
+    explicit ConcreteObserver2(std::shared_ptr<ObservableVector<T>> sp_ov) noexcept : Observer<T>::Observer{ sp_ov } {}
 protected:
     virtual void update(const Notification& n) const noexcept override
     {
         auto id{ Observer<T>::get_id() };
         auto& ov{ Observer<T>::get_observable_vector() };
-        std::cout << "[Observer " << id << "] received notification " << n << "\n";
-        std::cout << "[Observer " << id << "] sum of elements of observable vector: "
+        std::cout << "\t[Observer " << id << "] received notification " << n << "\n";
+        std::cout << "\t[Observer " << id << "] sum of elements of observable vector " << ov.get_id() << ": "
             << std::accumulate(std::begin(ov), std::end(ov), static_cast<T>(0)) << "\n";
     }
 };
@@ -134,39 +135,43 @@ class Subject
 public:
     virtual ~Subject() = default;
 
-    void attach(const std::shared_ptr<Observer<T>> observer) noexcept { observers_.insert( std::end(observers_), observer); }
+    [[nodiscard]] size_t get_id() const noexcept { return id_; }
+
+    void attach(const std::shared_ptr<Observer<T>> observer) noexcept
+    {
+        observers_.insert( std::end(observers_), observer);
+    }
     void detach(const std::shared_ptr<Observer<T>> observer) noexcept
     {
         observers_.remove_if([&observer](auto sp) { return sp->get_id() == observer->get_id(); });
     }
+
 protected:
     constexpr Subject() : id_{ id_generator_("Subject") } {}
     
-    // Copy operations don't copy the list of observers
-    // Move operations receive ownership of the list of observers
+    // Copy and move operations don't copy neither the id nor the list of observers
     constexpr Subject(const Subject& other) {}
-    constexpr Subject(Subject&& other) noexcept : id_{ std::move(other.id_) }, observers_{std::move(other.observers_)} {}
+    constexpr Subject(Subject&& other) noexcept { other.id_ = static_cast<size_t>(-1); other.observers_.clear(); }
     constexpr Subject& operator=(const Subject& other) { return *this; }
     constexpr Subject& operator=(Subject&& other)
     {
         if (this != &other)
         {
-            id_ = other.id_;
-            observers_.clear();
-            observers_ = std::move(other.observers_);
+            other.id_ = static_cast<size_t>(-1); other.observers_.clear();
         }
         return *this;
     }
 
-    [[nodiscard]] size_t get_id() const noexcept { return id_; }
-    void set_id(size_t id) noexcept { id_ = id; }
-
+    [[nodiscard]] bool is_observed() const noexcept { return not observers_.empty(); }
+    
     void notify(const Notification& n) const
     {
         std::for_each(std::begin(observers_), std::end(observers_), [&n](auto sp) { sp->update(n); });
     }
+
 private:
-    static inline IdGenerator& id_generator_{ IdGenerator::get_instance() };
+    static inline GroupIdGenerator& id_generator_{ GroupIdGenerator::get_instance() };
+
     size_t id_{};
     std::list<std::shared_ptr<Observer<T>>> observers_{};
 };
@@ -176,9 +181,12 @@ template <typename T>
 class ObservableVector : public Subject<T>
 {
 public:
+    using size_type = std::vector<T>::size_type;
+    using difference_type = std::vector<T>::difference_type;
+    using reference = std::vector<T>::reference;
+    using const_reference = std::vector<T>::const_reference;
     using iterator = std::vector<T>::iterator;
     using const_iterator = std::vector<T>::const_iterator;
-    using difference_type = std::vector<T>::difference_type;
 
     constexpr ObservableVector() noexcept = default;
     constexpr explicit ObservableVector(size_t s) : v_(s) {}
@@ -187,16 +195,17 @@ public:
     constexpr ObservableVector(InputIt first, InputIt last) : v_(first, last) {}
     constexpr ObservableVector(std::initializer_list<T> init) : v_(init) {}
     
-    constexpr ObservableVector(const ObservableVector& other) : Subject<T>(other), v_{ other.v_ } {}
-    constexpr ObservableVector(ObservableVector&& other) noexcept
-        : Subject<T>(std::move(other)), v_ { std::move(other.v_) } {}
+    constexpr ObservableVector(const ObservableVector& other) : Subject<T>{ other }, v_{ other.v_ } {}
+    constexpr ObservableVector(ObservableVector&& other) noexcept : Subject<T>{ std::move(other) }, v_{ std::move(other.v_) } {}
     constexpr ObservableVector& operator=(const ObservableVector& other)
     {
         if (this != &other)
         {
+            Subject<T>::operator=(other);
             v_.clear();
             v_ = other.v_;
-            Subject<T>::notify(Notification{ NotificationType::COPY_ASSIGNMENT });
+
+            if (Subject<T>::is_observed()) { Subject<T>::notify(Notification{ NotificationType::COPY_ASSIGNMENT }); }
         }
         return *this;
     }
@@ -204,28 +213,33 @@ public:
     {
         if (this != &other)
         {
+            Subject<T>::operator=(std::move(other));
             v_.clear();
             v_ = std::move(other.v_);
-            Subject<T>::operator=(std::move(other));
-            Subject<T>::notify(Notification{ NotificationType::MOVE_ASSIGNMENT });
+
+            if (Subject<T>::is_observed()) { Subject<T>::notify(Notification{ NotificationType::MOVE_ASSIGNMENT }); }
         }
         return *this;
     }
-    void push_back(T&& t)
+    constexpr reference operator[](size_type pos) { return v_[pos]; }
+    constexpr const_reference operator[](size_type pos) const { return v_[pos]; }
+    constexpr void push_back(T&& t)
     {
         v_.push_back(std::forward<T>(t));
-        Subject<T>::notify(Notification{ NotificationType::PUSH_BACK, v_.size() - 1 });
+
+        if (Subject<T>::is_observed()) { Subject<T>::notify(Notification{ NotificationType::PUSH_BACK, v_.size() - 1 }); }
     }
-    void pop_back()
+    constexpr void pop_back()
     {
         v_.pop_back();
-        Subject<T>::notify(Notification{ NotificationType::POP_BACK, v_.size() });
+
+        if (Subject<T>::is_observed()) { Subject<T>::notify(Notification{ NotificationType::POP_BACK, v_.size() }); }
     }
-    void clear() noexcept
+    constexpr void clear() noexcept
     {
         v_.clear();
-        Subject<T>::set_id(static_cast<size_t>(-1));
-        Subject<T>::notify(Notification{ NotificationType::CLEAR });
+
+        if (Subject<T>::is_observed()) { Subject<T>::notify(Notification{ NotificationType::CLEAR }); }
     }
     [[nodiscard]] auto size () const noexcept { return v_.size(); }
     [[nodiscard]] auto empty() const noexcept { return v_.empty(); }
@@ -238,6 +252,7 @@ public:
     constexpr const_iterator cend() const noexcept { return v_.cend(); }
 
     friend std::ostream& operator<<(std::ostream& os, const ObservableVector<T>& v) { return os << v.v_; }
+
 private:
     std::vector<T> v_{};
 };
@@ -275,48 +290,59 @@ constexpr auto cend(const ObservableVector<T>& v) -> decltype(end(v)) { return e
 void problem_71_main()
 {
     // Test observable vector constructors
-    auto sp_ov_1{ std::make_shared<ObservableVector<std::string>>() };
-    auto sp_ov_2{ std::make_shared<ObservableVector<float>>(5) };
-    auto sp_ov_3{ std::make_shared<ObservableVector<double>>(3, 1.5) };
-    const char* cstr{ "Hello, universe!" };  auto sp_ov_4{ std::make_shared<ObservableVector<char>>(cstr + 4, cstr + 8) };
-    auto sp_ov_5{ std::make_shared<ObservableVector<int>>(std::initializer_list<int>{ 1, 1, 2, 3, 5 })};
-    
-    std::cout << "ov_1: " << *sp_ov_1 << "\n";
-    std::cout << "ov_2: " << *sp_ov_2 << "\n";
-    std::cout << "ov_3: " << *sp_ov_3 << "\n";
-    std::cout << "ov_4: " << *sp_ov_4 << "\n";
-    std::cout << "ov_5: " << *sp_ov_5 << "\n";
+    std::cout << "Creating the observable vectors:\n";
+    auto sp_ov_0{ std::make_shared<ObservableVector<std::string>>() };
+    auto sp_ov_1{ std::make_shared<ObservableVector<float>>(5) }; for (auto i = 0; i < sp_ov_1->size(); ++i) { (*sp_ov_1)[i] = 3.14f * i; }
+    auto sp_ov_2{ std::make_shared<ObservableVector<double>>(3, 1.5) };
+    const char* cstr{ "Hello, universe!" };  auto sp_ov_3{ std::make_shared<ObservableVector<char>>(cstr + 4, cstr + 8) };
+    auto sp_ov_4{ std::make_shared<ObservableVector<int>>()};
+    std::cout << "\tov_0: " << *sp_ov_0 << "\n";
+    std::cout << "\tov_1: " << *sp_ov_1 << "\n";
+    std::cout << "\tov_2: " << *sp_ov_2 << "\n";
+    std::cout << "\tov_3: " << *sp_ov_3 << "\n";
+    std::cout << "\tov_4: " << *sp_ov_4 << "\n";
     std::cout << "\n";
 
     // Concrete observers
-    auto sp_co_1{ std::make_shared<ConcreteObserver1<std::string>>(sp_ov_1) };
-    auto sp_co_3{ std::make_shared<ConcreteObserver2<double>>(sp_ov_3) };
-    auto sp_co_5{ std::make_shared<ConcreteObserver2<int>>(sp_ov_5) };
+    auto sp_co_0{ std::make_shared<ConcreteObserver1<std::string>>(sp_ov_0) };
+    auto sp_co_1{ std::make_shared<ConcreteObserver2<float>>(sp_ov_1) };
+    auto sp_co_2{ std::make_shared<ConcreteObserver2<double>>(sp_ov_2) };
+    auto sp_co_3{ std::make_shared<ConcreteObserver1<char>>(sp_ov_3) };
+    auto sp_co_4{ std::make_shared<ConcreteObserver2<int>>(sp_ov_4) };
 
     // Attach concrete observers to observable vectors
+    sp_ov_0->attach(sp_co_0);
     sp_ov_1->attach(sp_co_1);
+    sp_ov_2->attach(sp_co_2);
     sp_ov_3->attach(sp_co_3);
-    sp_ov_5->attach(sp_co_5);
+    sp_ov_4->attach(sp_co_4);
 
     // Test operations and notifications
-    sp_ov_1->push_back("Tush! Never tell me;"); sp_ov_1->push_back("I take it much unkindly."); std::cout << "\n";
+    std::cout << "Pushing back on ov_0:\n";
+    sp_ov_0->push_back("Tush! Never tell me;"); sp_ov_0->push_back("I take it much unkindly."); std::cout << "\n";
 
-    if (not sp_ov_3->empty())
-    {
-        auto ov_moved{ std::move(*sp_ov_3) };  // move operations receive ownership of the list of observers
-        ov_moved.pop_back();
-        ov_moved.clear();
-    }
+    std::cout << "Copy assigning from ov_3:\n";
+    ObservableVector<char> ov_char{};  // copy operations don't copy the list of observers
+    ov_char = *sp_ov_3;
+    ov_char.pop_back();
     std::cout << "\n";
 
-    if (not sp_ov_5->empty())
-    {
-        auto ov_copied{ *sp_ov_5 };  // copy operations don't copy the list of observers
-        ov_copied.pop_back();
-        ov_copied.clear();
-    }
+    std::cout << "Move assigning from ov_1:\n";
+    ObservableVector<float> ov_float{};  // move operations don't copy the list of observers
+    ov_float = std::move(*sp_ov_1);
+    ov_float.push_back(3.14f);
     std::cout << "\n";
 
-    sp_ov_1->detach(sp_co_1);
-    sp_ov_1->push_back("That thou, Iago, who hast had my purse.");
+    std::cout << "Copy assigning to ov_3:\n";
+    *sp_ov_3 = ov_char;
+    std::cout << "\n";
+
+    std::cout << "Move assigning to ov_4:\n";
+    *sp_ov_4 = ObservableVector<int>(std::initializer_list<int>{ 1, 1, 2, 3, 5 });
+    std::cout << "\n";
+
+    std::cout << "Detaching from ov_1:\n";
+    sp_ov_0->detach(sp_co_0);
+    sp_ov_0->push_back("That thou, Iago, who hast had my purse.");
+    std::cout << "\n";
 }
