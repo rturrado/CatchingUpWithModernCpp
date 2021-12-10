bbcdc8aed8f64134d9db93ad4f26a5a273537502
rturrado <rturrado@gmail.com>
2021-12-11 00:15:09 +0100

    - TheModernC++Challenge:   * Added pugixml_wrapper.h, and moved all pugixml wrapper functions and exception code there.

--- TheModernC++Challenge/src/Chapter9_DataSerialization/Problem73.cpp
+++ TheModernC++Challenge/src/Chapter9_DataSerialization/Problem73.cpp
@@ -1,4 +1,5 @@
 #include "Chapter9_DataSerialization.h"
+#include "Chapter9_DataSerialization/pugixml_wrapper.h"
 
 #include "pugixml.hpp"
 
@@ -7,90 +8,8 @@
 #include <cstddef>  // ptrdiff_t
 #include <filesystem>
 #include <iostream>  // cout
-#include <stdexcept>  // rutime_error
-#include <sstream>  // ostringstream
 #include <string>
 #include <vector>
-
-
-namespace rtc::pugixml
-{
-    // Runtime errors
-    struct AttributeError : public std::runtime_error
-    {
-        AttributeError(const std::string& name) : std::runtime_error{ message_ + name } {}
-    private:
-        static inline std::string message_{ "trying to access attribute " };
-    };
-
-    struct ChildError : public std::runtime_error
-    {
-        ChildError(const std::string& name) : std::runtime_error{ message_ + name } {}
-    private:
-        static inline std::string message_{ "trying to access child " };
-    };
-
-    struct AppendAttributeError : public std::runtime_error
-    {
-        AppendAttributeError(const std::string& name) : std::runtime_error{ message_ + name } {}
-    private:
-        static inline std::string message_{ "trying to append attribute " };
-    };
-
-    struct AppendChildError : public std::runtime_error
-    {
-        AppendChildError(const std::string& name) : std::runtime_error{ message_ + name } {}
-    private:
-        static inline std::string message_{ "trying to append child " };
-    };
-
-    struct LoadFromError : public std::runtime_error
-    {
-        LoadFromError(const std::string& description, std::ptrdiff_t offset) : std::runtime_error{ "" }
-        {
-            std::ostringstream oss{};
-            oss << "trying to load from: \"" << description << "\", at offset " << offset;
-            message_ = oss.str();
-        }
-        virtual const char* what() const noexcept override { return message_.c_str(); }
-    private:
-        static inline std::string message_{};
-    };
-
-
-    // pugixml function wrappers
-    auto attribute_or_throw(const pugi::xml_node& node, const char* name)
-    {
-        if (auto ret{ node.attribute(name) }) { return ret; }
-        else { throw AttributeError{ name }; }
-    }
-
-    auto child_or_throw(const pugi::xml_node& node, const char* name)
-    {
-        if (auto ret{ node.child(name) }) { return ret; }
-        else { throw ChildError{ name }; }
-    }
-
-    auto append_attribute_or_throw(pugi::xml_node& node, const char* name)
-    {
-        auto ret{ node.append_attribute(name) };
-        if (ret) { return ret; }
-        else { throw AppendAttributeError{ name }; }
-    }
-
-    auto append_child_or_throw(pugi::xml_node& root, const char* name)
-    {
-        auto ret{ root.append_child(name) };
-        if (ret) { return ret; }
-        else { throw AppendChildError{ name }; }
-    }
-
-    void load_file_or_throw(pugi::xml_document& doc, const char* path)
-    {
-        auto result{ doc.load_file(path) };
-        if (not result) { throw LoadFromError{ result.description(), result.offset }; }
-    }
-}  // namespace rtc::pugixml
 
 
 struct Role
