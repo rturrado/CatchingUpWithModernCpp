c5abc1ccaa36b71a3c05715a2ecbe2162e8eb518
rturrado <rturrado@gmail.com>
2021-12-27 17:41:00 +0100

    - TheModernC++Challenge:   * Added skeletons for problems 77 and 78.

--- TheModernC++Challenge/src/Chapter9_DataSerialization/Problem77.cpp
+++ TheModernC++Challenge/src/Chapter9_DataSerialization/Problem77.cpp
@@ -2,6 +2,27 @@
 
 #include <iostream>  // cout
 
+
+// Printing a list of movies to a PDF
+//
+// Write a program that can print to a PDF file a list of movies in a tabular form, with the following requirements:
+//
+//   - There must be a heading to the list with the content 'List of movies'.
+//     This must appear only on the first page of the document.
+//   - For each movie, it should display the title, the release year, and the length.
+//   - The title, followed by the release year in parentheses, must be left-aligned.
+//   - The length, in hours and minutes (for example, 2:12), must be right-aligned.
+//   - There must be a line above and below the movie listing on each page.
+//
+// Here is an example of such a PDF output:
+//
+//    List of movies
+//    ---------------------------------
+//    The Matrix (1999)                   2:16
+//    Forrest Gump (1994)                 2:22
+//    The Truman Show (1998)              1:43
+//    The Pursuit of Happyness (2006)     1:57
+//    Fight Club (1999)                   2:19
 void problem_77_main()
 {
     std::cout << "\n";
