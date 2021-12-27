378900601bc1dbe64d4685c9eff2ffb03a5a7bdb
rturrado <rturrado@gmail.com>
2021-12-27 17:41:00 +0100

    - TheModernC++Challenge:   * Added skeletons for problems 77 and 78.

--- TheModernC++Challenge/src/Chapter9_DataSerialization/Problem78.cpp
+++ TheModernC++Challenge/src/Chapter9_DataSerialization/Problem78.cpp
@@ -2,6 +2,12 @@
 
 #include <iostream>  // cout
 
+
+// Creating a PDF from a collection of images
+//
+// Write a program that can create a PDF document that contains images from a user-specified directory.
+// The images must be displayed one after another.
+// If an image does not fit on the remainder of a page, it must be placed on the next page.
 void problem_78_main()
 {
     std::cout << "\n";
