49f065cafd83465e3d61180d76368dc0494e0f9e
rturrado <rturrado@gmail.com>
2022-02-16 03:13:06 +0100

    - TheModernC++Challenge:   * Implemented problem 90.

--- TheModernC++Challenge/src/Chapter11_Cryptography/Problem90.cpp
+++ TheModernC++Challenge/src/Chapter11_Cryptography/Problem90.cpp
@@ -1,6 +1,209 @@
 #include "Chapter11_Cryptography.h"
 
+#include "RtcFilesystem.h"
+
+#include <algorithm>  // transform
+#include <array>
+#include <bitset>
+#include <cstdint>  // uint8_t
+#include <filesystem>
 #include <iostream>  // cout
+#include <iterator>  // back_inserter
+#include <unordered_map>
+#include <string>
+#include <string_view>
+#include <vector>
+
+
+class Base64
+{
+public:
+    using value_type = uint8_t;
+
+private:
+    using encoding_table_t = std::array<unsigned char, 64>;
+    using decoding_table_t = std::unordered_map<char, value_type>;
+    using octet_t = std::bitset<8>;
+    using octets_t = std::array<octet_t, 3>;
+    using sextet_t = std::bitset<6>;
+    using sextets_t = std::array<sextet_t, 4>;
+
+    static inline const encoding_table_t encoding_table_{
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+        'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+    };
+    static inline const unsigned char padding{ '=' };
+
+    static inline const decoding_table_t decoding_table_{
+        {'A', 0}, {'B', 1}, {'C', 2}, {'D', 3}, {'E', 4}, {'F', 5}, {'G', 6}, {'H', 7},
+        {'I', 8}, {'J', 9}, {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14}, {'P', 15},
+        {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19}, {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23},
+        {'Y', 24}, {'Z', 25}, {'a', 26}, {'b', 27}, {'c', 28}, {'d', 29}, {'e', 30}, {'f', 31},
+        {'g', 32}, {'h', 33}, {'i', 34}, {'j', 35}, {'k', 36}, {'l', 37}, {'m', 38}, {'n', 39},
+        {'o', 40}, {'p', 41}, {'q', 42}, {'r', 43}, {'s', 44}, {'t', 45}, {'u', 46}, {'v', 47},
+        {'w', 48}, {'x', 49}, {'y', 50}, {'z', 51}, {'0', 52}, {'1', 53}, {'2', 54}, {'3', 55},
+        {'4', 56}, {'5', 57}, {'6', 58}, {'7', 59}, {'8', 60}, {'9', 61}, {'+', 62}, {'/', 63}
+    };
+
+public:
+    [[nodiscard]] std::string encode(const std::vector<value_type>& data, bool use_padding = true) const
+    {
+        std::string ret{};
+
+        auto get_sextet = [](const std::bitset<24>& input_bs, size_t sextet_index) {
+            std::bitset<24> sextet_mask_bs{ 0x00'00'3f };
+            auto sextet_ul{ ((input_bs >> (18 - 6 * sextet_index)) & sextet_mask_bs).to_ulong() };
+            return sextet_t{ sextet_ul };
+        };
+        auto build_sextets_from_octets = [&get_sextet](const auto& octets) {
+            std::bitset<24> input_bs{
+                (static_cast<unsigned long long>(octets[0].to_ulong()) << 16) +
+                (static_cast<unsigned long long>(octets[1].to_ulong()) << 8) +
+                octets[2].to_ulong()
+            };
+            return sextets_t{
+                get_sextet(input_bs, 0),
+                get_sextet(input_bs, 1),
+                get_sextet(input_bs, 2),
+                get_sextet(input_bs, 3)
+            };
+        };
+
+        size_t i{ 0 };
+        for (; i + 3 <= data.size(); i += 3)
+        {
+            auto octets{ octets_t{ data[i], data[i + 1], data[i + 2] } };
+            auto sextets{ build_sextets_from_octets(octets) };
+            std::ranges::transform(sextets, std::back_inserter(ret),
+                [](auto& sextet) { return encoding_table_[sextet.to_ulong()]; });
+        }
+
+        if (i < data.size())
+        {
+            auto bytes_left_to_read{ data.size() - i };
+
+            // 1 or 2 bytes left to read
+            octets_t octets{
+                octet_t{ data[i] },
+                (bytes_left_to_read == 2) ? octet_t{ data[i + 1] } : octet_t{},
+                octet_t{}
+            };
+            auto sextets{ build_sextets_from_octets(octets) };
+
+            // Add last sextets of information
+            //
+            // 1 byte left to read -> 2 sextets of information
+            // 2 bytes left to read -> 3 sextets of information
+            std::transform(std::cbegin(sextets), std::cbegin(sextets) + bytes_left_to_read + 1, std::back_inserter(ret),
+                [](auto& sextet) { return encoding_table_[sextet.to_ulong()]; });
+
+            // Add padding
+            //
+            // 1 byte left to read -> 2 sextets of padding
+            // 2 bytes left to read -> 1 sextet of padding
+            ret += use_padding
+                ? std::string(3 - bytes_left_to_read, padding)
+                : "";
+        }
+
+        return ret;
+    }
+
+    [[nodiscard]] std::vector<value_type> decode(std::string_view data) const
+    {
+        std::vector<value_type> ret{};
+
+        auto get_octet = [](const std::bitset<24>& input_bs, size_t octet_index) {
+            std::bitset<24> octet_mask_bs{ 0x00'00'ff };
+            auto octet_ul{ ((input_bs >> (16 - 8 * octet_index)) & octet_mask_bs).to_ulong() };
+            return octet_t{ octet_ul };
+        };
+        auto build_octets_from_sextets = [&get_octet](const std::bitset<24>& input_bs) {
+            return octets_t{
+                get_octet(input_bs, 0),
+                get_octet(input_bs, 1),
+                get_octet(input_bs, 2)
+            };
+        };
+        auto decode_group = [&build_octets_from_sextets, &ret](std::string_view group) {
+            std::bitset<24> input_bs{
+                (static_cast<unsigned long long>(decoding_table_.at(group[0])) << 18) +
+                (static_cast<unsigned long long>(decoding_table_.at(group[1])) << 12) +
+                (static_cast<unsigned long long>(decoding_table_.at(group[2])) << 6) +
+                decoding_table_.at(group[3])
+            };
+            auto octets{ build_octets_from_sextets(input_bs) };
+            std::ranges::transform(octets, std::back_inserter(ret),
+                [](auto& octet) { return static_cast<value_type>(octet.to_ulong()); });
+        };
+        auto decode_subgroup = [&build_octets_from_sextets, &ret](std::string_view subgroup) {
+            auto characters_left_to_read{ subgroup.size()};
+
+            // 2 or 3 characters left to read
+            std::bitset<24> input_bs{
+                (static_cast<unsigned long long>(decoding_table_.at(subgroup[0])) << 18) +
+                (static_cast<unsigned long long>(decoding_table_.at(subgroup[1])) << 12) +
+                ((characters_left_to_read == 3) ? (static_cast<unsigned long long>(decoding_table_.at(subgroup[2])) << 6) : 0)
+            };
+            auto octets{ build_octets_from_sextets(input_bs) };
+
+            // Add last octets of information
+            //
+            // 2 characters left to read -> 1 octet of information
+            // 3 characters left to read -> 2 octets of information
+            std::transform(std::cbegin(octets), std::cbegin(octets) + characters_left_to_read - 1, std::back_inserter(ret),
+                [](auto& octet) { return static_cast<value_type>(octet.to_ulong()); });
+        };
+
+        // Safety check for empty data
+        if (data.size() == 0)
+        {
+            return ret;
+        }
+        // Process everything except for the last group of characters
+        size_t i{ 0 };
+        for (; i + 4 < data.size(); i += 4)
+        {
+            decode_group({ std::cbegin(data) + i, std::cbegin(data) + i + 4});
+        }
+        // Process last group of characters
+        if (i + 4 == data.size())  // size is 4
+        {
+            // Check if it contains padding characters
+            if (auto end_pos{ data.find_first_of(padding, i) }; end_pos == std::string_view::npos)
+            {
+                decode_group({ std::cbegin(data) + i, std::cend(data) });  // no
+            }
+            else
+            {
+                decode_subgroup({ std::cbegin(data) + i, std::cbegin(data) + end_pos });  // yes
+            }
+        }
+        else // size is less than 4
+        {
+            decode_subgroup({ std::cbegin(data) + i, std::cend(data) });
+        }
+
+        return ret;
+    }
+};
+
+
+void test_base_64(const auto& input_data, bool use_padding = true)
+{
+    static const auto base64{ Base64{} };
+    const auto encoded_data{ base64.encode(input_data, use_padding) };
+    const auto decoded_data{ base64.decode(encoded_data) };
+
+    if (input_data == decoded_data) { std::cout << "\tOK.\n"; }
+    else { std::cout << "\tError: the decoded data differs from the input data.\n"; }
+}
 
 
 // Base64 encoding and decoding
@@ -10,5 +213,17 @@
 // The table used for encoding should be the one from the MIME specification.
 void problem_90_main()
 {
+    const auto input_file_path{ std::filesystem::current_path() / "res" / "fonts" / "calibri.ttf" };
+    const auto input_file_content{ rtc::filesystem::get_binary_file_content<Base64::value_type>(input_file_path) };
+    test_base_64(input_file_content);
+
+    for (std::string_view input_data : { "", "M", "Ma", "Man", "Many", "Many ", "Many h", "Many hands make light work." })
+    {
+        for (auto use_padding : { true, false })
+        {
+            test_base_64(std::vector<Base64::value_type>{std::cbegin(input_data), std::cend(input_data)}, use_padding);
+        }
+    }
+
     std::cout << "\n";
 }
